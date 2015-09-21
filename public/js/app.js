56305bc4030860a6642f7d583a4a2a679f8b40c3
Luis Sieira <luis.sieira@spacebar.fr>
2015-09-21 23:14:54 +0200

    beer retrieval abstracted to a service

--- public/js/app.js
+++ public/js/app.js
@@ -15,13 +15,11 @@
 			.when('/beer-detail', {
 				templateUrl : '/beer-detail',
 				controller  : 'productDetailController',
-				/**
-					TODO
-						This line should be :
-							resolve: { beer: beerRetrievalService.getRandomBeer }
-							you need to export the service as an object
-				*/
-				resolve: { beer: factory }
+
+				resolve: {
+					beer: function(beerRetrievalService) {
+					return beerRetrievalService.getRandomBeer();
+				}}
 			})
 			.when('/admin', {
 				templateUrl : '/admin',
@@ -38,29 +36,4 @@
 				templateUrl : '/404'
 			});
    }]);
-
-	 function factory($http, $q, $log) {
-			var deferred = $q.defer();
-			var beer = {};
-
-			$http.post('beer-detail', {})
-			.then(function(response) {
-				beer.beerDetail = response.data;
-
-				$http.get('beer-image/'+ beer.beerDetail.img, {})
-				.then(function(response) {
-					beer.beerImage = response.data;
-					deferred.resolve(beer);
-				}, function(err) {
-					$log.error('Error getting beer image', err);
-					deferred.reject({});
-				});
-			},
-			function(err) {
-				$log.error('Error getting random beer', err);
-				deferred.reject({});
-			});
-
-			return deferred.promise;
-		};
 })();
--- public/js/services.js
+++ public/js/services.js
@@ -4,48 +4,48 @@
   var app = angular.module('eltast')
 
   .factory('beerEditionService', function() {
-    var beerEditionService = {};
-
-    beerEditionService.prepareToEdit = function(beer) {
-      this.beer = beer;
-    }
-
-    beerEditionService.prepareToCreate = function() {
-      delete this.beer;
-    }
-
-    beerEditionService.getBeer = function() {
-      return this.beer || {};
-    }
-
-    return beerEditionService;
+    return {
+      prepareToEdit: function(beer) {
+        this.beer = beer;
+      },
+      prepareToCreate: function() {
+        delete this.beer;
+      },
+      getBeer: function() {
+        return this.beer || {};
+      },
+      getBeerImage: function() {
+        //TODO use the registered beer to get the image
+        return undefined;
+      }
+    };
   })
   .factory('beerRetrievalService', function ($http, $q, $log) {
-     beerRetrievalService.getRandomBeer = function() {
-       var deferred = $q.defer();
-       var beer = {};
+    return {
+      getRandomBeer: function() {
+        var deferred = $q.defer();
+        var beer = {};
 
-       $http.post('beer-detail', {})
-       .then(function(response) {
-         beer.beerDetail = response.data;
+        $http.post('beer-detail', {})
+        .then(function(response) {
+          beer.beerDetail = response.data;
 
-         $http.get('beer-image/'+ beer.beerDetail.img, {})
-         .then(function(response) {
-           beer.beerImage = response.data;
-           deferred.resolve(beer);
-         }, function(err) {
-           $log.error('Error getting beer image', err);
-           deferred.reject({});
-         });
-       },
-       function(err) {
-         $log.error('Error getting random beer', err);
-         deferred.reject({});
-       });
+          $http.get('beer-image/'+ beer.beerDetail.img, {})
+          .then(function(response) {
+            beer.beerImage = response.data;
+            deferred.resolve(beer);
+          }, function(err) {
+            $log.error('Error getting beer image', err);
+            deferred.reject({});
+          });
+        },
+        function(err) {
+          $log.error('Error getting random beer', err);
+          deferred.reject({});
+        });
 
-       return deferred.promise;
-     };
-
-     return beerRetrievalService;
+        return deferred.promise;
+      }
+    };
    });
 })();
